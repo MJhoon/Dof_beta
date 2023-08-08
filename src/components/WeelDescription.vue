89150ff4af5f3710e0d2d542b64a81272c1d1961
moonjihoon <s201064@naver.com>
2023-08-08 11:24:20 +0900

    www

---
+++ src/components/WeelDescription.vue
@@ -0,0 +1,173 @@
+<template>
+  <div class="popUp">
+    <div class="popUp-container" v-if="weelJobRequestVisible === false">
+      <div class="popUp-wrapper">
+        <div class="popUp-title-box">
+          <h4>{{ weelData.weelName }}</h4>
+          <span v-if="weelData.state === 'need_request'" style="background-color: #F19998;">요청 필요</span>
+          <span v-if="weelData.state === 'business_progress'" style="background-color: #ffc70b;">업무진행</span>
+          <span v-if="weelData.state === 'work_done'" style="background-color: #1CB44A;">업무완료</span>
+          <button></button>
+        </div>
+        <p class="popUp-sub-title">ADMIN 명 : {{ weelData.adminName }}</p>
+        <div class="popUp-text-box" :class="{ on: weelDetailVisible }">
+          <p>OWNER명 : {{ weelData.onnerName }}</p>
+          <p>OWNER 담당자명 : {{ weelData.onnerManagerName }}</p>
+          <p>WELL 계약 기간 : {{ weelData.weelTerm }}</p>
+					<div v-if="weelData.workRequest == true" style="color: #0473B5">
+						<p>담당 USER명 : {{ weelData.userManagerName }}</p>
+						<p>TYPE : {{ weelData.type }}</p>
+						<p>USER 계약 기간 : {{ weelData.userTerm }}</p>
+					</div>
+        </div>
+        <p class="btn-detail" @click="toggleDetail()"
+				>{{ weelDetailVisible ? "간략히 보기 >" : "WEEL 상세정보 >" }}</p>
+        <div class="btn-box-2">
+					<button @click="handleButtonClick" :style="{ backgroundColor: weelData.workRequest ? '#fff' : 'rgb(79, 79, 79)', color: weelData.workRequest ? '#000' : '#fff' }"> {{weelData.workRequest ? '요청 완료' : '업무 요청'}}</button>
+          <button style="background-color: #fff; color: #000">업무 확인</button>
+        </div>
+        <button class="btn-1">월간 리포트 {{weelData.report ? "수정" : "작성"}}</button>
+      </div>
+		<button class="btn-close-popup" @click="$emit('weelDataSend')"></button>
+    </div>
+		<WeelJobRequestVue v-if="weelJobRequestVisible === true" :weelName="weelData.weelName" :adminName="weelData.adminName"/>
+  </div>
+</template>
+
+<script>
+import WeelJobRequestVue from './WeelJobRequest.vue';
+
+export default {
+  data() {
+    return {
+      weelDescriptionOn: false,
+			weelDetailVisible: false,
+			weelJobRequestVisible: false,
+		};
+  },
+	components: {
+		WeelJobRequestVue,
+	},
+  props: {
+    weelData: Object,
+  },
+	methods: {
+		toggleDetail() {
+			this.weelDetailVisible = !this.weelDetailVisible;
+		},
+		handleButtonClick() {
+			if(!this.weelData.workRequest){
+				console.log('업무 요청 드가자');
+				this.weelJobRequestVisible = true;
+				this.weelJobRequest();
+			}
+		},
+		weelJobRequest() {
+
+		},
+	}
+};
+</script>
+
+<style>
+.popUp {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 9;
+  background-color: rgba(217, 217, 217, 0.4);
+}
+.popUp-container {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  border-radius: 19px;
+  background-color: #fff;
+}
+.popUp-wrapper {
+  position: relative;
+  padding: 28px 19px 19px;
+}
+.popUp-title-box {
+  display: flex;
+  align-items: center;
+	margin-bottom: 12px;
+}
+.popUp-title-box h4 {
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 24px;
+}
+.popUp-title-box span {
+  width: 75px;
+  height: 24px;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 24px;
+  text-align: center;
+	margin: 0 9px 0 17px;
+  border-radius: 30px;
+  background-color: #ffc70b;
+}
+.popUp-title-box button {
+}
+.btn-close-popup {
+  position: absolute;
+  right: 19px;
+  top: 15px;
+  width: 25px;
+  height: 24px;
+  background: url(../assets/btn-close-popup.png) no-repeat;
+}
+.popUp-sub-title {
+  text-align: left;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 22px;
+  color: rgba(84, 89, 94, 0.6);
+}
+.popUp-text-box {
+  height: 0;
+  overflow: hidden;
+  text-align: left;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 22px;
+}
+.popUp-text-box.on {
+	font-weight: "Bold";
+  height: auto;
+}
+.btn-detail {
+  text-align: left;
+  font-weight: 400;
+  font-size: 12px;
+  font-weight: 14.5px;
+  cursor: pointer;
+  margin: 8px 0 30px;
+  color: #000;
+}
+.btn-box-2 {
+  display: flex;
+  justify-content: space-between;
+}
+.btn-box-2 button {
+  width: calc(50% - 5px);
+  height: 44px;
+  border-radius: 8px;
+  border: 1px solid rgb(79, 79, 79);
+  background-color: rgb(79, 79, 79);
+  color: #fff;
+}
+.btn-1 {
+  width: 100%;
+  height: 44px;
+  border-radius: 8px;
+  border: 1px solid rgb(79, 79, 79);
+  background-color: rgb(79, 79, 79);
+  color: #fff;
+}
+</style>
---
+++ src/components/WeelJobRequest.vue
@@ -0,0 +1,178 @@
+<template>
+<div>
+  <div class="popUp-container">
+    <div class="popUp-wrapper">
+      <div class="popUp-title-box">
+        <h4 v-if="workStep == 0">{{ weelName }} 업무 요청</h4>
+        <h4 v-if="workStep == 1">{{ weelName }} 즉시 업무</h4>
+        <h4 v-if="workStep == 2">{{ weelName }} 기간 업무</h4>
+      </div>
+      <p class="popUp-sub-title">ADMIN 명 : {{ adminName }}</p>
+      <div class="popUp-content-box">
+        <div v-if="workStep === 0">
+          <button class="btn-1" @click="workStep = 1">즉시 요청</button>
+          <button class="btn-1" @click="workStep = 2">기간 요청</button>
+        </div>
+        <div v-if="workStep === 1">
+          <p style="margin-bottom: 17px; font-weight: 700; font-size: 14px">
+            업무 종류를 선택해주세요(복수 선택 가능)
+          </p>
+          <form @submit.prevent="submitForm">
+            <div class="btn-box-4" style="margin-bottom: 12px">
+              <input id="repair" type="checkbox" value="repair" v-model="workCheck"/>
+              <label for="repair"> 수리 </label>
+              <input id="inspection" type="checkbox" value="inspection" v-model="workCheck"/>
+              <label for="inspection"> 점검 </label>
+              <input id="oil-pickup" type="checkbox" value="oil-pickup" v-model="workCheck"/>
+              <label for="oil-pickup"> oil 픽업 </label>
+              <input id="water-pickup" type="checkbox" value="water-pickup" v-model="workCheck"/>
+              <label for="water-pickup"> 물 픽업 </label>
+            </div>
+            <div class="btn-box-2">
+              <button @click="resetForm">취소</button>
+              <button type="submit">요청</button>
+            </div>
+          </form>
+        </div>
+        <div v-if="workStep === 2">
+					<form @submit.prevent="submitFormDate">
+						<date-picker v-model="value1" type="date" range placeholder="시작일 ~ 종료일"></date-picker>
+						<!-- <date-picker v-model="value1" value-type="format" format="YYYY-MM-DD"></date-picker>
+						<date-picker v-model="value2" value-type="format" format="YYYY-MM-DD"></date-picker> -->
+            <div class="btn-box-4" style="margin-bottom: 12px">
+              <input id="repair" type="checkbox" value="repair" v-model="workCheck"/>
+              <label for="repair"> 수리 </label>
+              <input id="inspection" type="checkbox" value="inspection" v-model="workCheck"/>
+              <label for="inspection"> 점검 </label>
+              <input id="oil-pickup" type="checkbox" value="oil-pickup" v-model="workCheck"/>
+              <label for="oil-pickup"> oil 픽업 </label>
+              <input id="water-pickup" type="checkbox" value="water-pickup" v-model="workCheck"/>
+              <label for="water-pickup"> 물 픽업 </label>
+            </div>
+            <div class="btn-box-2">
+              <button @click="resetForm">취소</button>
+              <button type="submit">요청</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+    <button class="btn-close-popup" @click="$emit('weelDataSend')"></button>
+  </div>
+	<Modal>
+		<template></template>
+	</Modal>
+</div>
+</template>
+
+<script>
+import Modal from "./ModalForm.vue"
+import DatePicker from 'vue2-datepicker';
+import 'vue2-datepicker/index.css';
+
+export default {
+  data() {
+    return {
+      workStep: 0,
+			workCheck: [],
+			value1: [],
+    };
+  },
+	methods: {
+    submitForm() {
+      console.log('선택한 작업:', this.workCheck);
+      console.log('선택한 날짜:', this.value3);
+			if(this.workCheck.length === 0){
+				alert('업무의 종류를 선택해주세요.');
+			}else{
+
+			}
+    },
+		submitFormDate() {
+      console.log('선택한 작업:', this.workCheck);
+      console.log('선택한 날짜:', this.value1);
+			if(this.workCheck.length === 0){
+				alert('업무의 종류를 선택해주세요.');
+			}else if(this.value3.length === 0){
+				alert('업무의 종류 및 기간을 선택해주세요.');
+			}
+    },
+    resetForm() {
+      this.workCheck = [];
+      this.value1 = [];
+			this.workStep = 0
+    },
+  },
+	components: {
+		DatePicker,
+		Modal,
+	},
+  props: {
+    weelName: String,
+    adminName: String,
+  },
+};
+</script>
+
+<style>
+.popUp-content-box {
+  padding-top: 51px;
+  text-align: left;
+}
+.btn-box-4 {
+  display: flex;
+  justify-content: space-between;
+}
+.btn-box-4 label {
+  width: 23%;
+  height: 44px;
+  border-radius: 8px;
+  border: 1px solid rgb(79, 79, 79);
+  background-color: #fff;
+  text-align: center;
+  line-height: 44px;
+  color: #000;
+}
+.btn-box-4 input {
+  display: none;
+}
+input[id="repair"]:checked + label {
+  border: 1px solid rgb(79, 79, 79);
+  background-color: rgb(79, 79, 79);
+  color: #fff;
+}
+input[id="inspection"]:checked + label {
+  border: 1px solid rgb(79, 79, 79);
+  background-color: rgb(79, 79, 79);
+  color: #fff;
+}
+input[id="oil-pickup"]:checked + label {
+  border: 1px solid rgb(79, 79, 79);
+  background-color: rgb(79, 79, 79);
+  color: #fff;
+}
+input[id="water-pickup"]:checked + label {
+  border: 1px solid rgb(79, 79, 79);
+  background-color: rgb(79, 79, 79);
+  color: #fff;
+}
+
+/*date-picer css override*/
+.mx-input-wrapper{
+	margin-bottom: 31px;
+}
+.mx-datepicker-main{
+	top: 0;
+	left: 0;
+	width: 100%;
+}
+
+.mx-calendar-panel-date{
+	width: 100%;
+}
+.mx-calendar-panel-date:nth-child(1){
+	display: none;
+}
+
+
+</style>
